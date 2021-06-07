3fee2265a80b2eb5a23ed00b9c443ed2f322016b
mahmoudsiliman75 <mahmoudsiliman75@gmail.com>
2021-06-07 16:36:44 +0200

    finish personal index page first comments

--- PersonalIndex.php
+++ PersonalIndex.php
@@ -21,27 +21,29 @@
 
           <div class="kt-portlet__head filter">
             <form class="w_100">
-              <div class="row justify-content-center mt-4">
+              <div class="row justify-content-between my-4">
 
-                <div class="form-group col-4 mt-0">
-                  <div class="row">
-                    <div class="col-12">
-                      <div class="input-group-prepend col-12">
-                        <span class="input-group-text"> <i class="la la-star-o" style="font-size: 18px"></i> </span>
-                        <select class="form-control kt-selectpicker" data-size="7" data-live-search="true">
-                          <option> اختار شهر التقييم  </option>
-                          <option value="1"> فبراير 2021 </option>
-                          <option value="2"> مايو 2021 </option>
-                          <option value="3"> اغسطس 2021 </option>
-                        </select>
+                <div class="wraper col-6 col-md-6 d-flex justify-content-start align-items-center">
+                  <div class="form-group mb-0 w_80">
+                    <div class="row">
+                      <div class="col-12">
+                        <div class="input-group-prepend col-12">
+                          <span class="input-group-text"> <i class="la la-star-o" style="font-size: 18px"></i> </span>
+                          <select class="form-control kt-selectpicker" data-size="7" data-live-search="true">
+                            <option> اختار شهر التقييم  </option>
+                            <option value="1"> فبراير 2021 </option>
+                            <option value="2"> مايو 2021 </option>
+                            <option value="3"> اغسطس 2021 </option>
+                          </select>
+                        </div>
                       </div>
                     </div>
                   </div>
-                </div>
 
-                <div class="form-group col-2 mb-0">
-                  <div class="input-group">
-                    <button type="submit" class="btn btn-success no_border">بحث</button> 
+                  <div class="form-group mb-0">
+                    <div class="input-group">
+                      <button type="submit" class="btn btn-success no_border">بحث</button> 
+                    </div>
                   </div>
                 </div>
               </div>
@@ -50,97 +52,101 @@
 
           <!--START: RATING QUESTIONS TABLE-->
           <div class="container-fluid">
-            <table class="table mt-5">
-              <thead class="thead-dark">
-                <tr>
-                  <th style="width: 30px;">#</th>
-                  <th class="text-center"> السؤال </th>
-                  <th class="text-center"> التقييم </th>
+            <div class="row justify-content-center">
+              <div class="col-12 col-md-10">
+                <table class="table mt-5">
+                  <thead class="thead-dark">
+                    <tr>
+                      <th style="width: 30px;">#</th>
+                      <th class="text-center"> السؤال </th>
+                      <th class="text-center" style="width: 220px;"> التقييم </th>
 
-                </tr>
-              </thead>
+                    </tr>
+                  </thead>
 
-              <tbody>
-                <tr class="text-center">
-                  <td>1</td>
-                  <td> 
-                    لوريم ايبسوم دولار سيت أميت ,كونسيكتيتور أدايبا يسكينج أليايت,سيت دو أيوسمود تيمبور  
-                    أنكايديديونتيوت لابوري ات دولار ماجنا أليكيوا . يوت انيم أد مينيم؟
-                  </td>
-                  <td>
-                    <div class="wraper text-right">
-                      <span> <i class="la la-star la-2x kt-font-warning"></i> </span>
-                      <span> <i class="la la-star la-2x kt-font-warning"></i> </span>
-                      <span> <i class="la la-star la-2x kt-font-warning"></i> </span>
-                      <span> <i class="la la-star la-2x kt-font-warning"></i> </span>
-                      <span> <i class="la la-star la-2x kt-font-warning"></i> </span>
-                    </div>
-                  </td>
-                </tr>
+                  <tbody>
+                    <tr class="text-center">
+                      <td>1</td>
+                      <td> 
+                        لوريم ايبسوم دولار سيت أميت ,كونسيكتيتور أدايبا يسكينج أليايت,سيت دو أيوسمود تيمبور  
+                        أنكايديديونتيوت لابوري ات دولار ماجنا أليكيوا . يوت انيم أد مينيم؟
+                      </td>
+                      <td>
+                        <div class="wraper text-right">
+                          <span> <i class="la la-star la-2x kt-font-warning"></i> </span>
+                          <span> <i class="la la-star la-2x kt-font-warning"></i> </span>
+                          <span> <i class="la la-star la-2x kt-font-warning"></i> </span>
+                          <span> <i class="la la-star la-2x kt-font-warning"></i> </span>
+                          <span> <i class="la la-star la-2x kt-font-warning"></i> </span>
+                        </div>
+                      </td>
+                    </tr>
 
-                <tr class="text-center">
-                  <td>2</td>
-                  <td> 
-                    لوريم ايبسوم دولار سيت أميت ,كونسيكتيتور أدايبا يسكينج أليايت,سيت دو أيوسمود تيمبور  
-                    أنكايديديونتيوت لابوري ات دولار ماجنا أليكيوا . يوت انيم أد مينيم؟
-                  </td>
-                  <td>
-                    <div class="wraper text-right">
-                      <span> <i class="la la-star-half-o la-2x kt-font-warning"></i> </span>
-                      <span> <i class="la la-star la-2x kt-font-warning"></i> </span>
-                      <span> <i class="la la-star la-2x kt-font-warning"></i> </span>
-                      <span> <i class="la la-star la-2x kt-font-warning"></i> </span>
-                    </div>
-                  </td>
-                </tr>
+                    <tr class="text-center">
+                      <td>2</td>
+                      <td> 
+                        لوريم ايبسوم دولار سيت أميت ,كونسيكتيتور أدايبا يسكينج أليايت,سيت دو أيوسمود تيمبور  
+                        أنكايديديونتيوت لابوري ات دولار ماجنا أليكيوا . يوت انيم أد مينيم؟
+                      </td>
+                      <td>
+                        <div class="wraper text-right">
+                          <span> <i class="la la-star-half-o la-2x kt-font-warning"></i> </span>
+                          <span> <i class="la la-star la-2x kt-font-warning"></i> </span>
+                          <span> <i class="la la-star la-2x kt-font-warning"></i> </span>
+                          <span> <i class="la la-star la-2x kt-font-warning"></i> </span>
+                        </div>
+                      </td>
+                    </tr>
 
-                <tr class="text-center">
-                  <td>3</td>
-                  <td> 
-                    لوريم ايبسوم دولار سيت أميت ,كونسيكتيتور أدايبا يسكينج أليايت,سيت دو أيوسمود تيمبور  
-                    أنكايديديونتيوت لابوري ات دولار ماجنا أليكيوا . يوت انيم أد مينيم؟
-                  </td>
-                  <td>
-                    <div class="wraper text-right">
-                      <span> <i class="la la-star la-2x kt-font-warning"></i> </span>
-                      <span> <i class="la la-star la-2x kt-font-warning"></i> </span>
-                      <span> <i class="la la-star la-2x kt-font-warning"></i> </span>
-                      <span> <i class="la la-star la-2x kt-font-warning"></i> </span>
-                    </div>
-                  </td>
-                </tr>
+                    <tr class="text-center">
+                      <td>3</td>
+                      <td> 
+                        لوريم ايبسوم دولار سيت أميت ,كونسيكتيتور أدايبا يسكينج أليايت,سيت دو أيوسمود تيمبور  
+                        أنكايديديونتيوت لابوري ات دولار ماجنا أليكيوا . يوت انيم أد مينيم؟
+                      </td>
+                      <td>
+                        <div class="wraper text-right">
+                          <span> <i class="la la-star la-2x kt-font-warning"></i> </span>
+                          <span> <i class="la la-star la-2x kt-font-warning"></i> </span>
+                          <span> <i class="la la-star la-2x kt-font-warning"></i> </span>
+                          <span> <i class="la la-star la-2x kt-font-warning"></i> </span>
+                        </div>
+                      </td>
+                    </tr>
 
-                <tr class="text-center">
-                  <td>4</td>
-                  <td> 
-                    لوريم ايبسوم دولار سيت أميت ,كونسيكتيتور أدايبا يسكينج أليايت,سيت دو أيوسمود تيمبور  
-                    أنكايديديونتيوت لابوري ات دولار ماجنا أليكيوا . يوت انيم أد مينيم؟
-                  </td>
-                  <td>
-                    <div class="wraper text-right">
-                      <span> <i class="la la-star-half-o la-2x kt-font-warning"></i> </span>
-                      <span> <i class="la la-star la-2x kt-font-warning"></i> </span>
-                    </div>
-                  </td>
-                </tr>
+                    <tr class="text-center">
+                      <td>4</td>
+                      <td> 
+                        لوريم ايبسوم دولار سيت أميت ,كونسيكتيتور أدايبا يسكينج أليايت,سيت دو أيوسمود تيمبور  
+                        أنكايديديونتيوت لابوري ات دولار ماجنا أليكيوا . يوت انيم أد مينيم؟
+                      </td>
+                      <td>
+                        <div class="wraper text-right">
+                          <span> <i class="la la-star-half-o la-2x kt-font-warning"></i> </span>
+                          <span> <i class="la la-star la-2x kt-font-warning"></i> </span>
+                        </div>
+                      </td>
+                    </tr>
 
-                <tr class="text-center">
-                  <td>5</td>
-                  <td> 
-                    لوريم ايبسوم دولار سيت أميت ,كونسيكتيتور أدايبا يسكينج أليايت,سيت دو أيوسمود تيمبور  
-                    أنكايديديونتيوت لابوري ات دولار ماجنا أليكيوا . يوت انيم أد مينيم؟
-                  </td>
-                  <td>
-                    <div class="wraper text-right">
-                      <span> <i class="la la-star la-2x kt-font-warning"></i> </span>
-                      <span> <i class="la la-star la-2x kt-font-warning"></i> </span>
-                      <span> <i class="la la-star la-2x kt-font-warning"></i> </span>
-                      <span> <i class="la la-star la-2x kt-font-warning"></i> </span>
-                    </div>
-                  </td>
-                </tr>
-              </tbody>
-            </table>  
+                    <tr class="text-center">
+                      <td>5</td>
+                      <td> 
+                        لوريم ايبسوم دولار سيت أميت ,كونسيكتيتور أدايبا يسكينج أليايت,سيت دو أيوسمود تيمبور  
+                        أنكايديديونتيوت لابوري ات دولار ماجنا أليكيوا . يوت انيم أد مينيم؟
+                      </td>
+                      <td>
+                        <div class="wraper text-right">
+                          <span> <i class="la la-star la-2x kt-font-warning"></i> </span>
+                          <span> <i class="la la-star la-2x kt-font-warning"></i> </span>
+                          <span> <i class="la la-star la-2x kt-font-warning"></i> </span>
+                          <span> <i class="la la-star la-2x kt-font-warning"></i> </span>
+                        </div>
+                      </td>
+                    </tr>
+                  </tbody>
+                </table>  
+              </div>
+            </div>
           </div>
           <!--END: RATING QUESTIONS TABLE--> 
 
