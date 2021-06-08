3e132af73da021ac1dea0264b76c10535f5937cf
mahmoudsiliman75 <mahmoudsiliman75@gmail.com>
2021-06-08 13:06:51 +0200

    add progrss bar and users counter

--- PersonalIndex.php
+++ PersonalIndex.php
@@ -5,6 +5,31 @@
 	<!-- START:: CONTENT -->
 	<div class="kt-container  kt-container--fluid  kt-grid__item kt-grid__item--fluid">
     <div class="row">
+      <!-- START:: PROGRESS BAR SECTION -->
+      <div class="col-12">
+        <div class="kt-portlet px-4 min_height_auto">
+          <div class="row justify-content-between">
+            <div class="kt-section__content py-4 col-6">
+              <div class="progress">
+                <div class="progress-bar progress-bar-striped bg-success" role="progressbar" aria-valuenow="85" aria-valuemin="0" aria-valuemax="100" style="width: 85%"></div>
+              </div>
+            </div>
+
+            <div class="col-3 d-flex justify-content-end align-items-center">
+              <div class="persons_counter ltr_dir">
+                <span>
+                  <i class="la la-user la-2x"></i>
+                </span>
+                <span class="fs_25 margin">
+                  : 30  / 40
+                </span>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <!-- END:: PROGRESS BAR SECTION -->
+
       <!-- START:: RATINGS TABLES ECTION -->
       <div class="col-12">
         <div class="kt-portlet">
@@ -23,7 +48,7 @@
             <form class="w_100">
               <div class="row justify-content-between my-4">
 
-                <div class="wraper col-6 col-md-6 d-flex justify-content-start align-items-center">
+                <div class="wraper col-12 col-md-6 d-flex justify-content-start align-items-center">
                   <div class="form-group mb-0 w_80">
                     <div class="row">
                       <div class="col-12">
@@ -44,6 +69,25 @@
                     <div class="input-group">
                       <button type="submit" class="btn btn-success no_border">بحث</button> 
                     </div>
+                  </div>
+                </div>
+
+                <div class="wraper col-12 col-md-6 d-flex justify-content-end align-items-center">
+                  <div class="rating_avg">
+                    <span> <i class="la la-star-half-o la-2x kt-font-warning"></i> </span>
+                    <span> <i class="la la-star la-2x kt-font-warning"></i> </span>
+                    <span> <i class="la la-star la-2x kt-font-warning"></i> </span>
+                    <span> <i class="la la-star la-2x kt-font-warning"></i> </span>
+                    <span> <i class="la la-star la-2x kt-font-warning"></i> </span>
+                  </div>
+
+                  <div class="persons_counter ltr_dir m_end_15">
+                    <span>
+                      <i class="la la-user la-2x"></i>
+                    </span>
+                    <span class="fs_25 margin">
+                      : 10
+                    </span>
                   </div>
                 </div>
               </div>
